18da81a6f53480684adf1bbbfe7be701d7c16c1d
İsa Kaan Erdoğan <isakaanerdogan@gmail.com>
2020-03-25 02:58:42 +0300

    8kyu worth kata added.

---
+++ 8kyu/Function 1 - hello world/index.js
@@ -0,0 +1,16 @@
+/*
+  Title:
+    Function 1 - hello world
+  Description:
+    Make a simple function called greet that returns the most-famous "hello world!".
+    Style Points
+    Sure, this is about as easy as it gets. But how clever can you be to create
+    the most creative hello world you can think of? What is a "hello world"
+    solution you would want to show your friends?
+  Kata Link:
+    https://www.codewars.com/kata/function-1-hello-world
+*/
+
+function greet() {
+    return "hello world!";
+  }
